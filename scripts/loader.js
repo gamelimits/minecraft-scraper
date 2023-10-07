f5943b33915a588b8fb0d8e4ed8b5a0f702139e4
Kevin van der Burgt <kevin@binarylab.nl>
2023-10-07 11:49:37 +0200

    chore: initial commit

---
+++ scripts/loader.js
@@ -0,0 +1,7 @@
+import { register } from 'node:module';
+import { pathToFileURL } from 'node:url';
+import { setUncaughtExceptionCaptureCallback } from 'node:process';
+
+register('ts-node/esm', pathToFileURL('./'));
+
+setUncaughtExceptionCaptureCallback(console.log);
